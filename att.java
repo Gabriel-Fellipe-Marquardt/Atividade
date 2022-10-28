855dabb911a735f86c029c693e816d3abb370800
Gabriel-Fellipe-Marquardt <114319019+Gabriel-Fellipe-Marquardt@users.noreply.github.com>
2022-10-28 16:26:09 -0300

    código finalizado

--- att.java
+++ att.java
@@ -1,1 +1,22 @@
-"# Atividade" 
+import java.util.Scanner;
+
+public class att {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numero = 1, soma = 0;
+
+        while (numero != 0) {
+            System.out.println("Digite um número inteiro e positivo\nDigite 0 para finalizar o programa\n> ");
+            numero = scan.nextInt();
+            if (numero < 0) {
+                System.out.println("Negativos não entrarão no calculo");
+            } else if (numero > 0) {
+                soma += numero;
+            }
+            ;
+        }
+        System.out.println("O resultado é " + soma);
+    }
+
+}
